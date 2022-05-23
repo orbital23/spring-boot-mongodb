b7f39dbdbaa34748bb04a90d543e41663d40982d
Paul Cabrera Perez <paubrera23@gmail.com>
2022-05-23 10:43:15 -0500

    first commit

---
+++ src/main/java/com/pauldev/springbootmongodb/model/Tutorial.java
@@ -0,0 +1,23 @@
+package com.pauldev.springbootmongodb.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "tutorials")
+@Data
+@NoArgsConstructor
+public class Tutorial {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private boolean published;
+
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+}
---
+++ src/main/java/com/pauldev/springbootmongodb/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package com.pauldev.springbootmongodb.repository;
+
+import com.pauldev.springbootmongodb.model.Tutorial;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+    List<Tutorial> findByTitleContaining(String title);
+    List<Tutorial> findByPublished(boolean published);
+}
